971bb5ecedf15cf07c643feb4ad002fe63766cac
Tomek Wcislo <tomek@macbook-air-tomek.home>
2020-11-12 17:07:30 +0100

    setup production webpack build

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,6 @@ import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
 const now = moment();
-console.log(now.format('MMMM Do, YYYY'))
 
 export default class ExpenseForm extends React.Component {
     constructor(props) {
