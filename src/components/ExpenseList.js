afa718aafe631094c460ec620c39d310d4a2d6ca
Tomek Wcislo <tomek@macbook-air-tomek.home>
2020-11-14 20:09:22 +0100

    currency and date formatting implementation

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,13 +5,17 @@ import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
-        {props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />
-        })}
-        <ExpenseListItem />
+      {
+        props.expenses.length === 0 ? (
+          <p>No expenses</p>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
     </div>
-);
+  );
 
 const mapStateToProps = (state) => {
     return {
